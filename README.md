28482291a6ef8a528054575c9d07b283a5465ed1
Michael Hottman <hottmanmichael@gmail.com>
2016-07-27 10:32:15 -0600

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+#Basic React Setup
+
+###To open:
+1. `npm install`
+2. `npm run webpack`
+3. $ http-server -p 3000 -o
+
+###Flow
+* Webpack bundles react code into bundle.js file and throws it into public folder
+* scripts/main.jsx instructs react App to be injected into the #id div in public/index.html
---
+++ scripts/App.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var App = React.createClass({
+    render: function(){
+        return(
+            <div className="template">
+                <h1>Hello World!</h1>
+                <h3>Open up scripts/App.jsx to get started!</h3>
+            </div>
+        )
+    }
+});
+
+module.exports = App;
